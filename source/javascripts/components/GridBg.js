2caae6f29393c11826bef5e372f5f29cdd049d62
Julien Dargelos <julien.dargelos@me.com>
2018-04-11 15:12:39 +0200

    Fixed the grid

--- source/javascripts/components/GridBg.js
+++ source/javascripts/components/GridBg.js
@@ -24,7 +24,7 @@ export default {
   },
 
   init: function() {
-    this._hidden = false;
+    this._hidden = true;
     this.svg = document.querySelector('#grid-bg')
     this.lines = this.svg.querySelectorAll('.grid-bg__line');
 
